52810cea527c10134eda718fb79127c565325a34
yzlhyk <yzlhyk@gmail.com>
2021-03-11 18:56:34 +0800

    modified for production env

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -1,5 +1,18 @@
 import axios from "axios"
 
+// NODE_ENV = 'development'
+// NODE_ENV = 'production'
+
+// IF we are in production baseURL = /api/v1/restaurants
+// ELSE baseURL = "http://localhost:4000/api/v1/restaurants"
+
+// const baseURL = "http://localhost:4000/api/v1/restaurants"
+
+const baseURL = process.env.NODE_ENV === 'production' 
+    ? "api/v1/restaurants" 
+    : "http://localhost:4000/api/v1/restaurants"
+
+
 export default axios.create({
-  baseURL: "http://localhost:4000/api/v1/restaurants"
+  baseURL 
 })
